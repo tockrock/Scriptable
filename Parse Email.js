4d960a1559227330fc1d503421b00d13af3a6356
Takeshi Suzuki <tockrock@gmail.com>
2020-10-30 16:48:36 +0900

    Handle in a function

--- Parse Email.js
+++ Parse Email.js
@@ -1,19 +1,21 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: blue; icon-glyph: envelope; share-sheet-inputs: plain-text;
+// Editable from below.
 // copied from sample code
 
-let summary = args.plainTexts.length
-  + " texts\n"
-  + args.images.length
-  + " images\n"
-  + args.urls.length
-  + " URLs\n"
-  + args.fileURLs.length
-  + " file URLs"
+
 let input_string = args.plainTexts[0]
-let alert = new Alert()
-alert.title = "Shared"
-alert.message = input
-alert.addCancelAction("OK")
-await alert.presentAlert()
+
+let alert = new Alert();
+careco_handler(alert, input_string)
+await alert.presentAlert();
+
+function careco_handler(alert, email_content) {
+
+	alert.title = "Shared";
+	alert.message = email_content;
+	alert.addCancelAction("OK");
+
+}
+
